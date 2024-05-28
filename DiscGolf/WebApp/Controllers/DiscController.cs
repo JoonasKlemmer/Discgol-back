6682a47948cc2c2a6991ed443f7c44142a86a947
joklem <joklem@gmail.com>
2024-05-28 22:34:38 +0300

    Desktopis töötab

--- DiscGolf/WebApp/Controllers/DiscController.cs
+++ DiscGolf/WebApp/Controllers/DiscController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
-
+//muutus
 namespace WebApp.Controllers
 {
     public class DiscController : Controller
