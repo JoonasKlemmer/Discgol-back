bf8db12d830e0f1c42759b6744af4a9365c8866c
joklem <joklem@gmail.com>
2024-05-28 10:44:26 +0300

    .ReverseMap(); was missing from dal - bll

--- DiscGolf/App.BLL/AutoMapperProfile.cs
+++ DiscGolf/App.BLL/AutoMapperProfile.cs
@@ -7,7 +7,7 @@ public class AutoMapperProfile : Profile
         CreateMap<App.DAL.DTO.Category, App.BLL.DTO.Category>().ReverseMap();
         CreateMap<App.DAL.DTO.Wishlist, App.BLL.DTO.Wishlist>().ReverseMap();
         CreateMap<App.DAL.DTO.Disc, App.BLL.DTO.Disc>().ReverseMap();
-        CreateMap<App.DAL.DTO.DiscFromPage, App.BLL.DTO.DiscFromPage>();
+        CreateMap<App.DAL.DTO.DiscFromPage, App.BLL.DTO.DiscFromPage>().ReverseMap();
         CreateMap<App.DAL.DTO.DiscsInWishlist, App.BLL.DTO.DiscsInWishlist>().ReverseMap();
         CreateMap<App.DAL.DTO.Manufacturer, App.BLL.DTO.Manufacturer>().ReverseMap();
         CreateMap<App.DAL.DTO.Price, App.BLL.DTO.Price>().ReverseMap();
